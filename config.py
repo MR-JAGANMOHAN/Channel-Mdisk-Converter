43fa4f3239abcad4e639186060203c6b6b91eead
MR-JAGANMOHAN <107793234+MR-JAGANMOHAN@users.noreply.github.com>
2023-02-09 19:01:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,29 +9,29 @@ def is_enabled(value, default):
         return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID", "12124605"))
-API_HASH = os.environ.get("API_HASH", "5cf3577d85fd02286535ec2296934287")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5574279873:AAHb22nEOdC2dbwY_9GpU838frtUAvyFoQI")
-DROPLINK_API = os.environ.get("DROPLINK_API")
-MDISK_API = os.environ.get("MDISK_API", "Qu7jX9V0Sn3q1JHdxjPp")
-ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("ADMINS") else []
+API_ID = int(os.environ.get("API_ID", "13115322"))
+API_HASH = os.environ.get("API_HASH", "f28fbd1367ddda2e6f863c3129323743)
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5482257584:AAHndt0lola4y_6Fyu23rfOTQPp7p69VerA")
+DROPLINK_API = os.environ.get("6b341a7a92117e006798840ccb4a04e9a72c3879")
+MDISK_API = os.environ.get("MDISK_API", "zwxo6CxXnwc5vnNqj7PT")
+ADMINS = list(int(i.strip()) for i in os.environ.get("ADMINS").split(",")) if os.environ.get("1459910748") else []
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "MdiskConvertorBotpro")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://postbot:postbot@cluster0.ouwne8q.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Jagan:753753753@cluster0.zisdn.mongodb.net/?retryWrites=true&w=majority")
 WEBSITE = os.environ.get('WEBSITE')
 
 #  Optionnal variables
 INCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("INCLUDE_DOMAIN").split(",")) if os.environ.get("INCLUDE_DOMAIN") else []
 EXCLUDE_DOMAIN = list(i.strip() for i in os.environ.get("EXCLUDE_DOMAIN").split(",")) if os.environ.get("EXCLUDE_DOMAIN") else []
 CHANNELS = is_enabled((os.environ.get('CHANNELS', "True")), True)
-CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001806065563")) if os.environ.get("CHANNEL_ID") else []
+CHANNEL_ID = list(int(i.strip()) for i in os.environ.get("CHANNEL_ID").split("-1001806065563")) if os.environ.get("-1001842873695") else []
 FORWARD_MESSAGE = is_enabled((os.environ.get('FORWARD_MESSAGE', "True")), True)
-SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://github.com/CyniteOfficial/Channel-Mdisk-Converter")
-USERNAME = os.environ.get("USERNAME", "CyniteBackup")
+SOURCE_CODE = os.environ.get("SOURCE_CODE", "https://t.me/MR_JAGANMOHAN")
+USERNAME = os.environ.get("USERNAME", "MR_JAGANMOHAN)
 HEADER_TEXT = os.environ.get("HEADER_TEXT", '')
 FOOTER_TEXT = os.environ.get("FOOTER_TEXT", '')
 BANNER_IMAGE = os.environ.get("BANNER_IMAGE", '')
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '')
-LINK_BYPASS = is_enabled((os.environ.get('LINK_BYPASS', "False")), False)
+LINK_BYPASS = is_enabled((os.environ.get('LINK_BYPASS', "True")), False)
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
 HEROKU = True if HEROKU_API_KEY and HEROKU_APP_NAME else False
